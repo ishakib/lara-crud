b3a55a1f8f76eced88b305247cdbf416f925d78f
Shaki hossain <akm.shakib@gmail.com>
2023-11-14 03:43:46 +0600

    infra test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ishakib/lara-crud',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6e976c18800e492eb0d93a87aa81d2b21123da7e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ishakib/lara-crud' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6e976c18800e492eb0d93a87aa81d2b21123da7e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
