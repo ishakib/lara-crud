d7958588775d0ea4faa101708fb931c74baec514
Shaki hossain <akm.shakib@gmail.com>
2023-11-14 21:58:47 +0600

    dynamic files

--- src/Commands/LaraCrudCommand.php
+++ src/Commands/LaraCrudCommand.php
@@ -3,74 +3,101 @@
 namespace LaraCrud\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
 
 class LaraCrudCommand extends Command
 {
-    protected $signature = 'laracrud:generate {name}';
-    protected $description = 'Generate a complete CRUD operation';
+    protected $signature = 'lara:crud {model} {--directory= : Specify the directory for CRUD generation}';
+    protected $description = 'Generate model, migration, request, validation, route, and service for CRUD';
 
     public function handle()
     {
-        $name = $this->argument('name');
+        $modelName = $this->argument('model');
+        $pluralModelName = Str::plural(strtolower($modelName));
+        $directory = $this->option('directory');
 
-        // Create directories
-        $this->makeDirectories($name);
+        // Use the Laravel 'app' directory as the default if no directory is specified
+        $defaultDirectory = app_path();
 
-        // Generate files
-        $this->generateFiles($name);
+        // Check if the specified directory exists; if not, use the default directory
+        if ($directory && !is_dir($directory)) {
+            $this->error("The specified directory '{$directory}' does not exist. Using default directory: {$defaultDirectory}");
+            $directory = $defaultDirectory;
+        }
 
-        $this->info('CRUD generated successfully!');
+        $this->generateModel($modelName, $directory);
+        $this->generateMigration($modelName, $pluralModelName, $directory);
+        $this->generateRequest($modelName, $directory);
+        $this->generateValidation("App\Http\Requests\\{$modelName}Request", $directory);
+        $this->appendRoute($modelName, $pluralModelName, $directory);
+        $this->generateService($modelName, $directory);
+        $this->includeDemoControllerContent($modelName, $directory);
+
+        $this->info('CRUD code (excluding controller) generated successfully!');
     }
 
-    protected function makeDirectories($name)
+    protected function generateModel($modelName)
     {
-        File::makeDirectory(app_path("Http/Controllers/{$name}"), 0755, true, true);
-        File::makeDirectory(resource_path("views/{$name}"), 0755, true, true);
+        Artisan::call('make:model', ['name' => $modelName]);
     }
 
-    protected function generateFiles($name)
+    protected function generateMigration($modelName, $pluralModelName)
     {
-        // Generate Controller
-        $this->call('make:controller', [
-            'name' => "{$name}Controller",
+        Artisan::call('make:migration', [
+            'name' => "create_{$pluralModelName}_table",
+            '--create' => $pluralModelName,
         ]);
-
-        // Generate Model
-        $this->call('make:model', [
-            'name' => $name,
-        ]);
-
-        // Generate Views
-        $this->generateViews($name);
-
-        // Generate Validation Files
-        $this->generateValidationFiles($name);
-
-        // Generate Migration
-        $this->call('make:migration', [
-            'name' => "create_{$name}_table",
-        ]);
-
-        // Generate Routes
-        $this->generateRoutes($name);
     }
 
-    protected function generateViews($name)
+    protected function generateRequest($modelName)
     {
-        // You can customize this based on your view structure
-        File::copyDirectory(__DIR__.'/stubs/views', resource_path("views/{$name}"));
+        Artisan::call('make:request', ['name' => "{$modelName}Request"]);
     }
 
-    protected function generateValidationFiles($name)
+    protected function generateValidation($requestClassName, $directory = null)
     {
-        // You can customize this based on your validation needs
-        File::put(resource_path("lang/en/{$name}.php"), "<?php\n\nreturn [];");
+        $defaultDirectory = app_path('Http/Requests');
+        if ($directory && !is_dir($directory)) {
+            $this->error("The specified directory '{$directory}' does not exist. Using default directory: {$defaultDirectory}");
+            $directory = $defaultDirectory;
+        }
+
+        $validationContent = file_get_contents(resource_path('stubs/validation.stub'));
+
+        $validationPath = $directory ? $directory . '/' . $requestClassName . '.php' : $defaultDirectory . '/' . $requestClassName . '.php';
+
+        file_put_contents($validationPath, $validationContent);
+    }
+    protected function appendRoute($modelName, $pluralModelName)
+    {
+        $routeContent = "\nRoute::resource('{$pluralModelName}', '{$modelName}Controller');";
+        file_put_contents(base_path('routes/web.php'), $routeContent, FILE_APPEND);
     }
 
-    protected function generateRoutes($name)
+    protected function generateService($modelName)
     {
-        // You can customize this based on your route structure
-        File::append(base_path('routes/web.php'), "\n// CRUD Routes for {$name}\n// Route::resource('{$name}', '{$name}Controller');\n");
+        $serviceStubPath = resource_path('stubs/service.stub');
+
+        if (!file_exists($serviceStubPath)) {
+            $this->error("Service stub file not found: {$serviceStubPath}");
+            return;
+        }
+
+        $serviceContent = file_get_contents($serviceStubPath);
+        file_put_contents(app_path("Services/{$modelName}Service.php"), $serviceContent);
+    }
+
+    protected function includeDemoControllerContent($modelName)
+    {
+        $demoControllerStubPath = resource_path('stubs/demo_controller.stub');
+
+        if (!file_exists($demoControllerStubPath)) {
+            $this->error("Demo Controller stub file not found: {$demoControllerStubPath}");
+            return;
+        }
+
+        $demoControllerContent = file_get_contents($demoControllerStubPath);
+        file_put_contents(app_path("Http/Controllers/DemoController.php"), $demoControllerContent);
     }
 }
